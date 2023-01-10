dc48d8b95899791bad3335b665167f843950bfb1
Manu Kaushik <codebymanuk@gmail.com>
2023-01-10 22:25:46 +0530

    Starting🕶️

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+function Home() {
+  return (
+    <h1>Hello Bro</h1>
+  )
+}
+
+export default Home
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Message extends Component {
+    render() {
+        return (
+            <h1>THis is a class Component</h1>
+        )
+    }
+}
+
+export default Message;
